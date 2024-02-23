6b701b58f2ce7da17aa0c0e479fcadf74cb197b0
Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
2024-02-23 16:26:38 +0100

    renamed the Stream types to PacketStream

--- src/Clash/Cores/Ethernet/PacketStream.hs
+++ src/Clash/Cores/Ethernet/PacketStream.hs
@@ -11,8 +11,8 @@ import qualified Data.Maybe as Maybe
 -- We bundled _tstrb, _tdest and _tuser into one big _tmeta field which holds metadata.
 -- We removed _tid.
 -- No _tvalid, we wrap the Stream into a Maybe where the Stream is Nothing when _tvalid is False.
-data StreamM2S (dataWidth :: Nat) (metaType :: Type)
-  = StreamM2S {
+data PacketStreamM2S (dataWidth :: Nat) (metaType :: Type)
+  = PacketStreamM2S {
   _data :: Vec dataWidth (Unsigned 8),
   _byte_enable :: Maybe (Vec dataWidth Bool),
   -- ^ If Nothing, the entire byte is enabled by default, otherwise signifies which bytes are enabled
@@ -24,20 +24,20 @@ data StreamM2S (dataWidth :: Nat) (metaType :: Type)
 
 -- Slave to master: only a simple signal which tells the master whether
 -- the slave is ready to receive data
-newtype StreamS2M = StreamS2M {
+newtype PacketStreamS2M = PacketStreamS2M {
   _ready :: Bool
   -- ^ If True, the slave is ready to receive data
 } deriving (Generic, Show, ShowX, Bundle)
 
-data Stream (dom :: Domain) (dataWidth :: Nat) (metaType :: Type)
+data PacketStream (dom :: Domain) (dataWidth :: Nat) (metaType :: Type)
 
-instance Protocol (Stream dom dataWidth metaType) where
-  type Fwd (Stream dom dataWidth metaType) = Signal dom (Maybe (StreamM2S dataWidth metaType))
-  type Bwd (Stream dom dataWidth metaType) = Signal dom StreamS2M
+instance Protocol (PacketStream dom dataWidth metaType) where
+  type Fwd (PacketStream dom dataWidth metaType) = Signal dom (Maybe (PacketStreamM2S dataWidth metaType))
+  type Bwd (PacketStream dom dataWidth metaType) = Signal dom PacketStreamS2M
 
-instance DfConv (Stream dom dataWidth metaType) where
-  type Dom (Stream dom dataWidth metaType) = dom
-  type FwdPayload (Stream dom dataWidth metaType) = StreamM2S dataWidth metaType
+instance DfConv (PacketStream dom dataWidth metaType) where
+  type Dom (PacketStream dom dataWidth metaType) = dom
+  type FwdPayload (PacketStream dom dataWidth metaType) = PacketStreamM2S dataWidth metaType
 
   toDfCircuit proxy = toDfCircuitHelper proxy s0 blankOtp stateFn where
     s0 = ()
@@ -47,6 +47,6 @@ instance DfConv (Stream dom dataWidth metaType) where
 
   fromDfCircuit proxy =  fromDfCircuitHelper proxy s0 blankOtp stateFn where
     s0 = ()
-    blankOtp = StreamS2M { _ready = False }
+    blankOtp = PacketStreamS2M { _ready = False }
     stateFn m2s ack _ 
-      = pure (StreamS2M {_ready = ack }, m2s, False)
+      = pure (PacketStreamS2M {_ready = ack }, m2s, False)
