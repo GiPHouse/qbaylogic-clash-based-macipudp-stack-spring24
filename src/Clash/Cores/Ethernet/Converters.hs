570fd4fca5eb2372141a58397bc4803b2c4dfae4
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2024-02-25 22:20:18 +0100

    refactor states of upconverter to flushing and filling

--- src/Clash/Cores/Ethernet/Converters.hs
+++ src/Clash/Cores/Ethernet/Converters.hs
@@ -1,64 +1,77 @@
 module Clash.Cores.Ethernet.Converters
   ( upConverter
+  , sampleOut
   ) where
 
 import Clash.Prelude
 import Data.Maybe
 
-data UpConverterSt (p :: Nat) (n :: Nat) = UpConverterSt (Vec p (BitVector n)) (Index (p + 1))
+data UpConverterState (p :: Nat) (n :: Nat) =
+  FillNext {
+    _buffer      :: Vec p (BitVector n),
+    _byteEnable  :: Vec p Bool,
+    _abortRaised :: Bool
+  }
+  |
+  FlushNext {
+    _buffer      :: Vec p (BitVector n),
+    _byteEnable  :: Vec p Bool,
+    _abortRaised :: Bool
+  }
   deriving (Generic, NFDataX)
--- ^ Up converter state, consisting of at most p (BitVector n)s and the number of of element
+-- ^ Upconverter state, consisting of at most p (BitVector n)s and a vector indicating which bytes are valid                                            
 
 upConverter
   :: forall (p :: Nat) (n :: Nat) (dom :: Domain).
   HiddenClockResetEnable dom
   => 1 <= n
   => 1 <= p
-  => KnownNat n
-  => KnownNat p
-  => Signal dom (Maybe (Bool, BitVector n))
+  => "_wordSize" ::: KnownNat n
+  => "_upconvStreamSize" ::: KnownNat p
+  => "_dataIn" ::: Signal dom (Maybe (Bool, BitVector n))
   -- ^ data stream, The bool indicates whether this is the last word of a packet
-  -> Signal dom Bool
+  -> "_abort" ::: Signal dom Bool
   -- ^ Error occured
-  -> Signal dom Bool
+  -> "_consumerReady" ::: Signal dom Bool
   -- ^ The consumer is ready to receive a word
-  -> ( Signal dom (Maybe (Vec p Bool))
-     , Signal dom (Maybe (Vec p (BitVector n)))
-     , Signal dom Bool
-     , Signal dom Bool
+  -> ( "_dataOut"      ::: Signal dom (Maybe (Vec p (BitVector n)))
+     , "_byteEnable"   ::: Signal dom (Maybe (Vec p Bool))
+     , "_abort"         ::: Signal dom Bool
+     , "_receiveReady" ::: Signal dom Bool
+
+-- _dataOut:     Upconverted stream. Is Nothing unless _consumer_ready && one upconverted word is ready
+-- _byteEnable:  If Nothing, all bytes in upconverted stream are enabled by default, otherwise signifies which bytes are enabled.
+-- _abort:        Error occurred
+-- _receiveReady: The upconverter is ready to receive a word
      )
-   -- ^ The upconverted stream. if the bool (last) is true then the index indicates
-   --   how many words are valid in the vec. E.g. If last && index == 0. then
-   --   it means only the first element in the vec contains a valid word
-   --
-   -- The second element is the error
-   --
-   -- The third element is whether the upConverter is ready to receive data.
-upConverter payload abort sinkRdy = case compareSNat (SNat @n) (SNat @1) of
-  SNatLE -> (fmap (fmap (fmap Clash.Prelude.repeat)) payload, abort, sinkRdy)
-  _ -> mealyB go s_0 (payload, abort, sinkRdy)
+
+upConverter dataIn0 abort0 consumerReady0 = mealyB go s_0 (dataIn0, abort0, consumerReady0)
     where
       go
-        :: UpConverterSt p n 
+        :: UpConverterState p n
         -> (Maybe (Bool, BitVector n), Bool, Bool)
-        -> (UpConverterSt p n, (Maybe (Bool, Vec p (BitVector n)), Bool, Bool))
+        -> (UpConverterState p n, (Maybe(Vec p (BitVector n)), Maybe(Vec p Bool), Bool, Bool))
 
-      go (UpConverterSt xs i) (payload', abort', sinkRdy') =
-        let notFull = i < natToNum @p
-            canWrite = notFull || sinkRdy'
-            (xs', add_i, last_be') = case (canWrite, payload') of
-              (True, Just (last_be, x)) -> (xs <<+ x, 1, last_be)
-              _ -> (xs, 0, False)
-            payload'' = if notFull || not last_be'
-              then Nothing
-              else Just (last_be', xs')
-            sub_i = if isJust payload''
-              then natToNum @n
-              else 0
-            i' = (i - sub_i) + add_i
-        in (UpConverterSt xs' i', (payload'', abort', canWrite))
+      go st@(UpConverterState buffer byteEnable abortRaised) (dataIn1, abort1, consReady1) =
+        case dataIn1 of
+          Nothing -> (st, (Nothing, Nothing, abortRaised, True))
+          Just (lastByte, word) ->
+            let isEmpty = head @(p-1) byteEnable
+                full  = last @(p-1) byteEnable
+                flush = full || lastByte
 
-      s_0 = UpConverterSt (repeat 0) 0
+                bufferNext = buffer <<+ word
+                byteEnableNext = if isEmpty then repeat 0 <<+ 1 else buffer <<+ 0
+
+                canWrite = (not full) || consReady1 -- If we are not full, or full but consumer can consume the stream, we can receive new data next cycle
+                
+                abortRaisedNext = (abortRaised || abort1) && (not isEmpty)
+
+                outStream = if flush then Just buffer else Nothing
+                
+            in (UpConverterState bufferNext byteEnableNext abortRaisedNext, outStream canWrite)
+
+      s_0 = UpConverterState (repeat 0) 0 (repeat 0)
 
 payloadInp :: [Maybe (Bool, BitVector 8)]
 payloadInp = [Nothing]
@@ -69,11 +82,19 @@ abortInp = [False]
 sinkReadyInp :: [Bool]
 sinkReadyInp = [False]
 
+clk :: Clock System
 clk = systemClockGen
+
+rst :: Reset System
 rst = systemResetGen
+
+en :: Enable dom
 en = enableGen
 
+payloadOut :: "data out, valid byte" ::: Signal System (Maybe (Vec 4 (BitVector 8), BitVector 4))
+abortOut :: "error out" ::: Signal System Bool
+sinkReadyOut :: "receive ready" ::: Signal System Bool
 (payloadOut, abortOut, sinkReadyOut) = exposeClockResetEnable (upConverter @4) clk rst en (fromList payloadInp) (fromList abortInp) (fromList sinkReadyInp)
 
-sampleOut 
+sampleOut :: [(Maybe (Vec 4 (BitVector 8), BitVector 4), Bool, Bool)]
 sampleOut = sampleN 60 $ bundle (payloadOut, abortOut, sinkReadyOut)
