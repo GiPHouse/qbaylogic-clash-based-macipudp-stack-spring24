8a590528d974172a22d74ece45351e952cb28ad9
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2024-02-26 18:23:13 +0100

    working function, also correct?

--- src/Clash/Cores/Ethernet/Converters.hs
+++ src/Clash/Cores/Ethernet/Converters.hs
@@ -4,18 +4,12 @@ module Clash.Cores.Ethernet.Converters
   ) where
 
 import Clash.Prelude
-import Data.Maybe
 
 data UpConverterState (p :: Nat) (n :: Nat) =
-  FillNext {
+  UpConverterState {
     _buffer      :: Vec p (BitVector n),
     _byteEnable  :: Vec p Bool,
-    _abortRaised :: Bool
-  }
-  |
-  FlushNext {
-    _buffer      :: Vec p (BitVector n),
-    _byteEnable  :: Vec p Bool,
+    _flushEarly  :: Bool,
     _abortRaised :: Bool
   }
   deriving (Generic, NFDataX)
@@ -38,12 +32,11 @@ upConverter
      , "_byteEnable"   ::: Signal dom (Maybe (Vec p Bool))
      , "_abort"         ::: Signal dom Bool
      , "_receiveReady" ::: Signal dom Bool
-
+  )
 -- _dataOut:     Upconverted stream. Is Nothing unless _consumer_ready && one upconverted word is ready
 -- _byteEnable:  If Nothing, all bytes in upconverted stream are enabled by default, otherwise signifies which bytes are enabled.
 -- _abort:        Error occurred
 -- _receiveReady: The upconverter is ready to receive a word
-     )
 
 upConverter dataIn0 abort0 consumerReady0 = mealyB go s_0 (dataIn0, abort0, consumerReady0)
     where
@@ -52,35 +45,49 @@ upConverter dataIn0 abort0 consumerReady0 = mealyB go s_0 (dataIn0, abort0, cons
         -> (Maybe (Bool, BitVector n), Bool, Bool)
         -> (UpConverterState p n, (Maybe(Vec p (BitVector n)), Maybe(Vec p Bool), Bool, Bool))
 
-      go st@(UpConverterState buffer byteEnable abortRaised) (dataIn1, abort1, consReady1) =
+      go st@(UpConverterState buffer byteEnable flushEarly abortRaised) (dataIn1, abort1, consReady1) =
         case dataIn1 of
           Nothing -> (st, (Nothing, Nothing, abortRaised, True))
           Just (lastByte, word) ->
-            let isEmpty = head @(p-1) byteEnable
-                full  = last @(p-1) byteEnable
-                flush = full || lastByte
+            let isFull  = last @(p-1) byteEnable
+                flush = isFull || flushEarly
+
+                flushEarlyNext = lastByte
 
                 bufferNext = buffer <<+ word
-                byteEnableNext = if isEmpty then repeat 0 <<+ 1 else buffer <<+ 0
+                byteEnableNext = if flush then repeat False <<+ True else byteEnable <<+ False -- reset byte-enable vector after flushing
 
-                canWrite = (not full) || consReady1 -- If we are not full, or full but consumer can consume the stream, we can receive new data next cycle
+                canWrite = (not isFull) || consReady1 -- If we are not full, or full but consumer can consume the stream, we can receive new data next cycle
                 
-                abortRaisedNext = (abortRaised || abort1) && (not isEmpty)
+                abortRaisedNext = (abortRaised || abort1) && (not flush) -- reset abort of state after flushing
 
-                outStream = if flush then Just buffer else Nothing
+                outStream = if flush then Just buffer else Nothing -- outstream only if flush
                 
-            in (UpConverterState bufferNext byteEnableNext abortRaisedNext, outStream canWrite)
+            in (UpConverterState 
+                  bufferNext 
+                  byteEnableNext 
+                  flushEarlyNext 
+                  abortRaisedNext, 
+                  (outStream, if isFull then Nothing else Just byteEnable, abortRaised, canWrite))
 
-      s_0 = UpConverterState (repeat 0) 0 (repeat 0)
+      s_0 = UpConverterState (repeat 0) (repeat False) False False
 
-payloadInp :: [Maybe (Bool, BitVector 8)]
-payloadInp = [Nothing]
-
+payloadInp :: [Maybe (Bool, BitVector 1)]
+payloadInp = [
+    Just (False,0b0 :: BitVector 1)
+  , Just (False,0b0 :: BitVector 1)
+  , Just (False,0b0 :: BitVector 1)
+  , Just (False,0b0 :: BitVector 1)
+  
+  , Just (False,0b0 :: BitVector 1)
+  , Just (False,0b0 :: BitVector 1)
+  , Just (True,0b0 :: BitVector 1)
+  , Just (True,0b0 :: BitVector 1)]
 abortInp :: [Bool]
-abortInp = [False]
+abortInp = [False, False, True, False, False, True, False, False]
 
 sinkReadyInp :: [Bool]
-sinkReadyInp = [False]
+sinkReadyInp = [False, True, True, True, False, False, False, True]
 
 clk :: Clock System
 clk = systemClockGen
@@ -91,10 +98,11 @@ rst = systemResetGen
 en :: Enable dom
 en = enableGen
 
-payloadOut :: "data out, valid byte" ::: Signal System (Maybe (Vec 4 (BitVector 8), BitVector 4))
-abortOut :: "error out" ::: Signal System Bool
-sinkReadyOut :: "receive ready" ::: Signal System Bool
-(payloadOut, abortOut, sinkReadyOut) = exposeClockResetEnable (upConverter @4) clk rst en (fromList payloadInp) (fromList abortInp) (fromList sinkReadyInp)
+payloadOut :: "_dataOut" ::: Signal System (Maybe (Vec 4 (BitVector 1)))
+byteEnOut :: "_byteEnable" ::: Signal System (Maybe (Vec 4 Bool))
+abortOut :: "_abort" ::: Signal System Bool
+sinkReadyOut :: "_receiveReady" ::: Signal System Bool
+(payloadOut, byteEnOut, abortOut, sinkReadyOut) = exposeClockResetEnable (upConverter @4) clk rst en (fromList payloadInp) (fromList abortInp) (fromList sinkReadyInp)
 
-sampleOut :: [(Maybe (Vec 4 (BitVector 8), BitVector 4), Bool, Bool)]
-sampleOut = sampleN 60 $ bundle (payloadOut, abortOut, sinkReadyOut)
+sampleOut :: [(Maybe (Vec 4 (BitVector 1)), Maybe(Vec 4 Bool), Bool, Bool)]
+sampleOut = sampleN 60 $ bundle (payloadOut, byteEnOut, abortOut, sinkReadyOut)
