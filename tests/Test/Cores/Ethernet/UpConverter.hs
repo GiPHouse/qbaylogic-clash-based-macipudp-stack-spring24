e0f0411996b6eb8e92a766da7618da7d065f17b7
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2024-03-08 17:17:50 +0100

    not ready, for collaboration

--- tests/Test/Cores/Ethernet/UpConverter.hs
+++ tests/Test/Cores/Ethernet/UpConverter.hs
@@ -7,6 +7,12 @@ module Test.Cores.Ethernet.UpConverter where
 -- base
 import Prelude
 
+-- list
+import qualified Data.List as L
+
+-- maybe
+import qualified Data.Maybe as M
+
 -- clash-prelude
 import qualified Clash.Prelude as C
 import Clash.Prelude (type (<=))
@@ -41,7 +47,7 @@ prop_packetstream_sometest_id =
     @C.System
     defExpectOptions
     (Gen.list (Range.linear 0 100) genPackets)
-    (C.exposeClockResetEnable $ go)
+    (C.exposeClockResetEnable $ model)
     (C.exposeClockResetEnable @C.System (ckt @4))
     (\a b -> a === b)
   where
@@ -52,64 +58,34 @@ prop_packetstream_sometest_id =
       => Circuit (PacketStream dom 1 ()) (PacketStream dom dataWidth ())
     ckt = upConverterC
 
-    go :: UpConverterState dataWidth
-      -> (Maybe (PacketStreamM2S 1 ()))
-      -> ( UpConverterState dataWidth
-         , Maybe (PacketStreamM2S dataWidth ())
-         )
-    go st@(UpConverterState {..}) (Nothing)
---   = (nextSt, (toPacketStream st))
---     where
---       -- If we can accept data we can always set _ucFlush to false,
---       -- since we only change state if we can transmit and receive data
---       nextStRaw = st
---                     { _ucFlush = False
---                     , _ucAborted = isNothing _ucLastIdx && _ucAborted
---                     , _ucLastIdx = Nothing
---                     }
---       outReady = not _ucFlush || inReady
---       nextSt = if outReady then nextStRaw else st
--- go st@(UpConverterState {..}) (Just (PacketStreamM2S{..}), PacketStreamS2M inReady)
---   = (nextSt, (PacketStreamS2M outReady, toPacketStream st))
---     where
---       inLast = isJust _last
---       -- We smear an abort over the entire rest of the packet
---       -- so the next abort is set:
---       --  - If fragment we are potentially flushing was not the last and we were already aborting;
---       --  - or if the incoming fragment is aborted
---       nextAbort = (isNothing _ucLastIdx && _ucAborted) || _abort
---       -- If we are not flushing we can accept data to be stored in _ucBuf,
---       -- but when we are flushing we can only accept if the current
---       -- output fragment is accepted by the sink
---       outReady = not _ucFlush || inReady
---       bufFull = _ucIdx == maxBound
---       nextBuf = replace _ucIdx (head _data) _ucBuf
+    model inpStream = PacketStreamM2S <$> outData <*> outLast <*> outMeta <*> outAbort where
 
---       nextFlush = inLast || bufFull
---       nextIdx = if nextFlush then 0 else _ucIdx + 1
+      chunks = go [] [] 0 inpStream where
+        go out _   _  []         = out
+        go out acc sz (pkt:pkts) = case sz of
+          4 -> go (out ++ [acc]) [] sz pkts
+          _ -> case _last pkt of
+            Nothing -> go out (acc ++ [pkt] ) (sz + 1) pkts
+            Just _  -> go (out ++ [acc ++ [pkt]]) [] 0 pkts
 
---       nextStRaw = UpConverterState
---                     { _ucBuf =  nextBuf
---                     , _ucIdx = nextIdx
---                     , _ucFlush = nextFlush
---                     , _ucAborted = nextAbort
---                     , _ucLastIdx = toMaybe inLast _ucIdx
---                     }
---       nextSt = if outReady then nextStRaw else st
 
-{-
-    upConv stream = reverse (go [] [] 0 stream)
-    go out acc accSize [] = acc:out
-    go out acc accSize (inp:inps) = 
-      case inp of
-        Nothing -> go acc accSize packets 
-        Just (str@(PacketStreamM2S {..})) -> case _last of
-          True  -> go (acc:out) [] 0 inps
-          False -> case accSize of
-            4 -> go (acc:out) acc accSize
-            _ -> go out acc (accSize + 1) inps
--}
-  -- This is used to generate
+      outMeta = replicate (length chunks)()
+      outAbort= fmap (or. fmap _abort) chunks
+      endsAbdruptly = fmap (M.isJust . _last . L.last) chunks
+      outLast = fmap (\(ealy, ch) -> if ealy then Just (fromIntegral $ length ch ) else Nothing ) (zip endsAbdruptly chunks)
+
+      padWithZeroes list = take 4 $ list ++ repeat 0
+      chunkToVec v [] = v
+      chunkToVec v (x:xs) = chunkToVec (v C.<<+ x) xs
+
+      listOfDataOfTheChunks = fmap (fmap  _data) chunks
+      listOfVecsOfTheChunks = fmap (fmap head) listOfDataOfTheChunks
+
+      outData = fmap (chunkToVec ( 0 C.:> 0 C.:> 0 C.:> 0 C.:> C.Nil) . padWithZeroes . fmap _data) chunks
+
+
+
+
     genPackets =
       PacketStreamM2S <$>
       (genVec Gen.enumBounded) <*>
