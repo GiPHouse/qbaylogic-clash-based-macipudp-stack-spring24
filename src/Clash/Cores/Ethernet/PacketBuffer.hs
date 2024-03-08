75bd09e8b15f6d56ce7a1bf648f03edea2f0c230
Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
2024-03-08 16:34:22 +0100

    make format

--- src/Clash/Cores/Ethernet/PacketBuffer.hs
+++ src/Clash/Cores/Ethernet/PacketBuffer.hs
@@ -16,26 +16,26 @@ packetBuffer
     => NFDataX metaType
     => 1 <= sizeBits
     => SNat sizeBits
-    -> ( Signal dom (Maybe (PacketStreamM2S dataWidth metaType)),  
+    -> ( Signal dom (Maybe (PacketStreamM2S dataWidth metaType)),
             Signal dom PacketStreamS2M
         )
-    -> (Signal dom (), Signal dom (Maybe (PacketStreamM2S dataWidth metaType))) 
+    -> (Signal dom (), Signal dom (Maybe (PacketStreamM2S dataWidth metaType)))
 
-packetBuffer depth (inM2S, inS2M) =  (pure (), outM2S)
+packetBuffer SNat (inM2S, inS2M) =  (pure (), outM2S)
     where
         --The backing ram
         outM2S = blockRam1 NoClearOnReset (SNat @(2 ^ sizeBits)) (errorX "initial block ram contents") readAddr' writeCommand
 
          -- write command
         writeCommand = mux writeEnable (Just <$> bundle (wordAddr, inM2S)) (pure Nothing)
-        
+
         --The read and write pointers
         wordAddr, packetAddr, readAddr :: Signal dom (Unsigned sizeBits)
         wordAddr = register 0 $ mux writeEnable (wordAddr + 1) wordAddr
         packetAddr = register 0 $ mux lastWord wordAddr packetAddr
         readAddr' = mux readEnable (readAddr + 1) readAddr
         readAddr = register 0 readAddr'
-        
+
         -- Only write if there is space
         writeEnable = writeRequest .&&. fmap not full .&&. fmap not dropping
         readEnable = (_ready <$> inS2M) .&&. fmap not emptyBuffer
@@ -43,7 +43,7 @@ packetBuffer depth (inM2S, inS2M) =  (pure (), outM2S)
         --The status signals
         emptyBuffer  = register 0 packetAddr .==. readAddr
         full   = readAddr .==. (packetAddr + 1)
-        dropping = register False ((full .&&. writeRequest) .||. (dropping .&&. fmap not lastWord)) 
+        dropping = register False ((full .&&. writeRequest) .||. (dropping .&&. fmap not lastWord))
         writeRequest = isJust <$> inM2S
         lastWord = isLast <$> inM2S
         isLast :: Maybe (PacketStreamM2S dataWidth metaType) -> Bool
