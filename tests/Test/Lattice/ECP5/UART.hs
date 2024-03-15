5402ce5b39723e18b95d97360184222c12ae6968
Akribes <30956441+Akribes@users.noreply.github.com>
2024-03-15 11:05:10 +0100

    Id test uart tx and rx

---
+++ tests/Test/Lattice/ECP5/UART.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Test.Lattice.ECP5.UART where
+
+-- base
+import Prelude
+import Data.Proxy
+
+-- clash-prelude
+import qualified Clash.Prelude as C
+import Clash.Prelude (type (<=))
+
+-- hedgehog
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+-- tasty
+import Test.Tasty
+import Test.Tasty.Hedgehog (HedgehogTestLimit(HedgehogTestLimit))
+import Test.Tasty.Hedgehog.Extra (testProperty)
+import Test.Tasty.TH (testGroupGenerator)
+
+-- clash-protocols
+import Protocols
+import Protocols.Internal
+import Protocols.Hedgehog
+
+-- Me
+import Clash.Cores.Ethernet.PacketStream
+import Clash.Lattice.ECP5.UART
+import qualified Protocols.DfConv as DfConv
+
+genVec :: (C.KnownNat n, 1 <= n) => Gen a -> Gen (C.Vec n a)
+genVec gen = sequence (C.repeat gen)
+
+unsafeToPacketStream :: Circuit (CSignal dom (Maybe (PacketStreamM2S 1 ()))) (PacketStream dom 1 ())
+unsafeToPacketStream = Circuit (\(CSignal fwdInS, _) -> (CSignal $ pure (), fwdInS))
+
+-- | Tests that data can be sent through UART, except for _abort and _last and _meta signals. Ignores backpressure.
+prop_uart_tx_rx_id :: Property
+prop_uart_tx_rx_id = idWithModelSingleDomain @C.System defExpectOptions gen (C.exposeClockResetEnable model) ckt
+  where
+    ckt = C.exposeClockResetEnable (
+      uartTxC @C.System (C.SNat @6250000) 
+      |> uartRxC (C.SNat @6250000) 
+      |> unsafeToPacketStream
+      |> DfConv.fifo (Proxy :: Proxy (PacketStream C.System 1 ())) (Proxy :: Proxy (PacketStream C.System 1 ())) (C.SNat @50)
+      )
+
+    model :: C.HiddenClockResetEnable dom => ExpectType (PacketStream dom 1 ()) -> ExpectType (PacketStream dom 1 ())
+    model = fmap (\x -> x {_abort=False, _last=Nothing})
+
+    gen :: Gen (ExpectType (PacketStream dom 1 ()))
+    gen = Gen.list (Range.linear 0 50) genPackets
+
+    genPackets =
+      PacketStreamM2S <$>
+      genVec Gen.enumBounded <*>
+      Gen.maybe Gen.enumBounded <*>
+      Gen.enumBounded <*>
+      Gen.enumBounded
+
+tests :: TestTree
+tests =
+    localOption (mkTimeout 12_000_000 {- 12 seconds -})
+  $ localOption (HedgehogTestLimit (Just 200))
+  $(testGroupGenerator)
--- tests/unittests.hs
+++ tests/unittests.hs
@@ -3,8 +3,10 @@ import Prelude
 import Test.Tasty
 
 import qualified Test.Cores.Ethernet.PacketStream
+import qualified Test.Lattice.ECP5.UART
 
 main :: IO ()
 main = defaultMain $ testGroup "."
   [ Test.Cores.Ethernet.PacketStream.tests
+  , Test.Lattice.ECP5.UART.tests
   ]
