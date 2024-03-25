9b472e44ec0acbbd5fee000fcd3ba351154f80a3
t-wallet <tw.personal@tutanota.com>
2024-03-25 17:18:34 +0100

    final style and comment changes

--- src/Clash/Cores/Ethernet/AsyncFIFO.hs
+++ src/Clash/Cores/Ethernet/AsyncFIFO.hs
@@ -9,15 +9,17 @@ import Protocols.Internal ( Circuit, fromSignals )
 import Clash.Cores.Ethernet.PacketStream
 
 
--- ^ Asynchronous FIFO circuit that can be used to safely cross clock domains.
+-- | Asynchronous FIFO circuit that can be used to safely cross clock domains.
 asyncFifoC :: forall (depth     :: Nat)
                      (dataWidth :: Nat)
                      (wDom      :: Domain)
                      (rDom      :: Domain)
                      (metaType  :: Type) .
   ( KnownDomain wDom, KnownDomain rDom
-  , KnownNat depth, 2 <= depth
-  , KnownNat dataWidth, 1 <= dataWidth
+  , KnownNat depth
+  , 2 <= depth
+  , KnownNat dataWidth
+  , 1 <= dataWidth
   , NFDataX metaType)
   => SNat depth
   -> Clock wDom
@@ -30,9 +32,9 @@ asyncFifoC :: forall (depth     :: Nat)
 asyncFifoC depth wClk wRst wEn rClk rRst rEn = fromSignals ckt where
   ckt (fwdIn, bwdIn) = (bwdOut, fwdOut) where
     (element, empty, full) = asyncFIFOSynchronizer depth wClk rClk wRst rRst wEn rEn readReq fwdIn
+    -- If the FIFO is empty, we output Nothing. Else, we output the oldest element.
     fwdOut = mux empty (pure Nothing) (Just <$> element)
-    -- ^ If the FIFO is empty, we output Nothing. Else, we output the oldest element.
+    -- Assert backpressure when the FIFO is full.
     bwdOut = PacketStreamS2M . not <$> full
-    -- ^ Assert backpressure when the FIFO is full.
-    readReq = not <$> empty .&&. _ready <$> bwdIn
-    -- ^ Next component is ready to read if the fifo is not empty and it does not assert backpressure.
+    -- Next component is ready to read if the fifo is not empty and it does not assert backpressure.
+    readReq = (not <$> empty) .&&. (_ready <$> bwdIn)
