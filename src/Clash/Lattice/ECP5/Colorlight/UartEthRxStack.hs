e08fcdc15f9272abae39b16b5d309bdabbea3f3c
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2024-03-28 18:50:44 +0100

    minimize constraints

--- src/Clash/Lattice/ECP5/Colorlight/UartEthRxStack.hs
+++ src/Clash/Lattice/ECP5/Colorlight/UartEthRxStack.hs
@@ -4,49 +4,42 @@ module Clash.Lattice.ECP5.Colorlight.UartEthRxStack
 ( uartEthRxStack
 ) where
 
-import Clash.Cores.Ethernet.PacketStream ( PacketStream, PacketStreamM2S(..) )
-import Clash.Cores.Ethernet.RGMII
-import Clash.Cores.Ethernet.RxStack ( rxStack )
-import Clash.Cores.UART
-import Clash.Lattice.ECP5.Colorlight.CRG
-import Clash.Lattice.ECP5.Prims
-import Clash.Lattice.ECP5.UART
+-- import prelude
 import Clash.Prelude
-import Clash.Signal ( HiddenClock, HiddenEnable, HiddenReset )
-import Data.Maybe ( fromJust )
-import Data.Proxy
-import Protocols
-import Protocols.Internal ( CSignal(CSignal) )
 
+-- import maybe, proxy
+import Data.Maybe ( fromJust )
+import Data.Proxy ( Proxy(Proxy) )
+
+-- import ethernet
+import Clash.Cores.Ethernet.PacketStream ( PacketStream, PacketStreamM2S(..) )
+import Clash.Cores.Ethernet.RGMII ( rgmiiReceiver, RGMIIRXChannel )
+import Clash.Cores.Ethernet.RxStack ( rxStack )
+
+-- import uart
+import Clash.Cores.UART ( ValidBaud, baudGenerator )
+
+-- import ECP5
+import Clash.Lattice.ECP5.Prims ( delayg, iddrx1f )
+import Clash.Lattice.ECP5.UART ( uartTxNoBaudGenC )
+
+-- import protocols
+import Protocols ( Circuit, toSignals, (|>) )
+import Protocols.Internal ( CSignal(CSignal) )
 import Protocols.DfConv ( fifo )
+
 
 extractSignal :: CSignal dom a -> Signal dom a
 extractSignal (CSignal x) = x
 
--- uartEthRxStack
---   :: forall (domEth :: Domain)
---             (domDDREth :: Domain)
---             (dom :: Domain)
---             (baud :: Nat)
---             fPeriod
---             edge
---             reset
---             init
---             polarity
---    . (KnownConfiguration domDDREth ('DomainConfiguration domDDREth fPeriod edge reset init polarity)
---    , HiddenClockResetEnable domEth
---    , HiddenClockResetEnable domDDREth
---    , (ValidBaud domEth baud))
---   => KnownConfiguration domEth ('DomainConfiguration domEth (2*fPeriod) edge reset init polarity)
 uartEthRxStack
-  :: forall baud dom domEth0 domDDREth0
-  . 
+  :: forall baud dom domEth0 domDDREth0. 
   ( KnownDomain domDDREth0
+  , HiddenClockResetEnable domEth0
   , ValidBaud dom baud
   , KnownConf domEth0 ~ 'DomainConfiguration domEth0 8000 'Rising 'Asynchronous 'Unknown 'ActiveHigh
-  , KnownConf domDDREth0 ~ 'DomainConfiguration
-                        domDDREth0 4000 'Rising 'Asynchronous 'Unknown 'ActiveHigh, (
-                       HiddenClockResetEnable domEth0,HiddenClockResetEnable dom, dom ~ domEth0 ))
+  , KnownConf domDDREth0 ~ 'DomainConfiguration domDDREth0 4000 'Rising 'Asynchronous 'Unknown 'ActiveHigh
+  , dom ~ domEth0 )
   => SNat baud
   -> "eth" ::: RGMIIRXChannel domEth0 domDDREth0
   -> "uart_tx" ::: Signal dom Bit
