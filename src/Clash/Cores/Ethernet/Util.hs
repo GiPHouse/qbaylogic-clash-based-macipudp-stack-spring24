68e55567c224647a8c603a2ed9184c3746379222
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2024-03-28 21:34:43 +0100

    change requests made in review

--- src/Clash/Cores/Ethernet/Util.hs
+++ src/Clash/Cores/Ethernet/Util.hs
@@ -1,16 +1,11 @@
 module Clash.Cores.Ethernet.Util
     ( toMaybe
-    , extractSignal
     ) where
 
 import Clash.Prelude
-import Protocols.Internal ( CSignal(CSignal) )
 
 -- | Wrap a value in a Just if True
 toMaybe :: Bool -> a -> Maybe a
 toMaybe True x = Just x
 toMaybe False _ = Nothing
 
--- | Extract Signal from CSignal
-extractSignal :: CSignal dom a -> Signal dom a
-extractSignal (CSignal x) = x
--- src/Clash/Lattice/ECP5/Colorlight/UartEthRxStack.hs
+++ src/Clash/Lattice/ECP5/Colorlight/UartEthRxStack.hs
@@ -8,21 +8,19 @@ module Clash.Lattice.ECP5.Colorlight.UartEthRxStack
 import Clash.Prelude
 
 -- import maybe, proxy
-import Data.Maybe ( fromJust )
 import Data.Proxy ( Proxy(Proxy) )
 
 -- import ethernet
 import Clash.Cores.Ethernet.PacketStream ( PacketStream, PacketStreamM2S(..) )
 import Clash.Cores.Ethernet.RGMII ( RGMIIRXChannel, rgmiiReceiver )
 import Clash.Cores.Ethernet.RxStack ( rxStack )
-import Clash.Cores.Ethernet.Util ( extractSignal )
 
 -- import uart
-import Clash.Cores.UART ( ValidBaud, baudGenerator )
+import Clash.Cores.UART ( ValidBaud )
 
 -- import ECP5
 import Clash.Lattice.ECP5.Prims ( delayg, iddrx1f )
-import Clash.Lattice.ECP5.UART ( uartTxNoBaudGenC )
+import Clash.Lattice.ECP5.UART ( uartTxC )
 
 -- import protocols
 import Protocols ( Circuit, toSignals, (|>) )
@@ -31,38 +29,29 @@ import Protocols.Internal ( CSignal(CSignal) )
 
 
 uartEthRxStack
-  :: forall baud dom domEth0 domDDREth0.
-  ( KnownDomain domDDREth0
-  , HiddenClockResetEnable domEth0
-  , ValidBaud dom baud
-  , KnownConf domEth0 ~ 'DomainConfiguration domEth0 8000 'Rising 'Asynchronous 'Unknown 'ActiveHigh
-  , KnownConf domDDREth0 ~ 'DomainConfiguration domDDREth0 4000 'Rising 'Asynchronous 'Unknown 'ActiveHigh
-  , dom ~ domEth0 )
+  :: forall baud domEth domDDREth.
+  ( KnownDomain domDDREth
+  , HiddenClockResetEnable domEth
+  , ValidBaud domEth baud
+  , KnownConf domEth ~ 'DomainConfiguration domEth 8000 'Rising 'Asynchronous 'Unknown 'ActiveHigh
+  , KnownConf domDDREth ~ 'DomainConfiguration domDDREth 4000 'Rising 'Asynchronous 'Unknown 'ActiveHigh
+  )
   => SNat baud
-  -> "eth" ::: RGMIIRXChannel domEth0 domDDREth0
-  -> "uart_tx" ::: Signal dom Bit
-uartEthRxStack baud channel = extractSignal uartTxBit
+  -> RGMIIRXChannel domEth domDDREth
+  -> Signal domEth Bit
+uartEthRxStack baud channel = uartTxBitS
   where
     rgmiiRx = rgmiiReceiver channel (delayg d80) iddrx1f
     packetStream = fmap rgmiiRecvToPacketStream rgmiiRx
 
-    ckt :: Circuit (PacketStream domEth0 1 ()) (CSignal dom Bit)
+    ckt :: Circuit (PacketStream domEth 1 ()) (CSignal domEth Bit)
     ckt = rxStack
       |> fifo Proxy Proxy (SNat @16000)
-      |> uartTxNoBaudGenC (baudGenerator baud)
+      |> uartTxC baud
 
-    (_, uartTxBit) = toSignals ckt (packetStream, CSignal $ pure ())
-
-
--- uartEthRxStack :: RGMIIRXChannel DomEth1 DomDDREth1 -> Signal dom Bit
--- uartEthRxStack channel = rgmiiReceiver channel _ _ |> DfConv.fifo Proxy Proxy (P.SNat @16000) |> uartTxNoBaudGenC
+    (_, CSignal uartTxBitS) = toSignals ckt (packetStream, CSignal $ pure ())
 
 rgmiiRecvToPacketStream :: (Bool, Maybe (BitVector 8)) -> Maybe (PacketStreamM2S 1 ())
-rgmiiRecvToPacketStream input =
-    let (err, dat) = input
-        dat' = fromJust dat
-    in case dat of
-  Nothing -> Nothing
-  _       -> Just $ PacketStreamM2S {_data = pure dat', _last = Nothing, _meta = (), _abort = err}
-
+rgmiiRecvToPacketStream (err, Just dat) = Just $ PacketStreamM2S (singleton dat) Nothing () err
+rgmiiRecvToPacketStream (_, Nothing) = Nothing
 
