87828192b498bb936ebf60abd9e8831c49a050ad
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2024-04-13 23:27:57 +0200

    test, state machine, cabal

---
+++ src/Clash/Cores/Ethernet/FcsInserter.hs
@@ -0,0 +1,229 @@
+{-# language FlexibleContexts #-}
+{-# language MultiParamTypeClasses #-}
+{-# language RecordWildCards #-}
+{-# language ScopedTypeVariables #-}
+{-# language TemplateHaskell#-}
+module Clash.Cores.Ethernet.FcsInserter
+( fcsInserter
+, fcsInserterC
+, x
+) where
+import Clash.Cores.Crc
+import Clash.Cores.Crc.Catalog
+import Control.Monad qualified as M
+
+
+import Clash.Cores.Ethernet.PacketStream
+import Clash.Cores.Ethernet.Util
+import Clash.Prelude
+import Clash.Prelude.Mealy ( mealy )
+import Clash.Sized.Vector ( unsafeFromList )
+import Data.Char ( ord )
+import Data.Data ( Proxy(Proxy) )
+import Data.List ( group )
+import Data.List qualified as List
+import Data.Maybe
+import Protocols
+
+
+bv2fragment
+  :: Vec 4 (BitVector 8)
+  -> PacketStreamM2S 4 ()
+bv2fragment vector = PacketStreamM2S vector (Just 4) () False
+
+fragment2bv
+    :: PacketStreamM2S 4 () -> (Index 4, Vec 4 (BitVector 8))
+fragment2bv (PacketStreamM2S{..}) = (fromMaybe maxBound _last, _data)
+
+-- | Like 'take' but uses a 'Data.Type.Ord.<=' constraint
+takeLe
+  :: forall (n :: Nat)
+            (m :: Nat)
+            a
+   . n <= m
+  => SNat n
+  -- ^ How many elements to take
+  -> Vec m a
+  -- ^ input vector
+  -> Vec n a
+takeLe SNat vs = leToPlus @n @m $ takeI vs
+
+appendVec
+  :: forall n m a
+   . KnownNat n
+  => Index n
+  -> Vec n a
+  -> Vec m a
+  -> Vec (n + m) a
+appendVec valid xs ys = results !! valid
+  where
+    go :: forall l. SNat l -> Vec (n + m) a
+    go l@SNat = let f = addSNat l d1 in case compareSNat f (SNat @n) of
+                  SNatLE -> takeLe (addSNat l d1) xs ++ ys ++ extra
+                   where
+                     extra :: Vec (n - (l + 1)) a
+                     extra = repeat undefined
+                  _ -> error "appendVec: Absurd"
+    results = smap (\s _ -> go s) xs
+
+x = appendVec 2 ('c' :> 'l' :> 'a' :> undefined :> Nil) ('s' :> 'h' :> Nil)
+
+data FcsInserterState
+  = FcsCopy
+      { _cachedFwd :: Maybe (PacketStreamM2S 4 ()) }
+  | FcsInsert
+      { _cachedFwd :: Maybe (PacketStreamM2S 4 ())
+      , _bytesRemaining :: Index 4
+      , _crc :: Vec 4 (BitVector 8)
+      }
+  deriving (Show, Generic, NFDataX)
+
+
+fcsInserter
+  :: forall (dom :: Domain)
+  .  HiddenClockResetEnable dom
+  => HardwareCrc Crc32_ethernet 8 4
+  => ( Signal dom (Maybe (PacketStreamM2S 4 ()))
+     , Signal dom PacketStreamS2M
+     )
+  -> ( Signal dom PacketStreamS2M
+     , Signal dom (Maybe (PacketStreamM2S 4 ()))
+     )
+fcsInserter (fwdIn, bwdIn) = (bwdOut, fwdOut)
+  where
+    fwdInX = fromJustX <$> fwdIn
+    crcInX = fragment2bv <$> fwdInX
+    transferOccured = ready .&&. isJust <$> fwdIn
+    crcIn = toMaybe <$> transferOccured <*> crcInX
+
+    firstFragment = regEn True transferOccured $ isJust . _last <$> fwdInX
+    ethCrc = crcEngine @dom (Proxy @Crc32_ethernet) firstFragment crcIn
+    ethCrcBytes = reverse . unpack <$> ethCrc
+
+    bwdOut = PacketStreamS2M <$> ready
+
+    (fwdOut, ready) = fcsHelper (ethCrcBytes, fwdIn, bwdIn)
+
+
+fcsInserterC
+  :: forall (dom :: Domain)
+  .  KnownDomain dom
+  => HiddenClockResetEnable dom
+  => HardwareCrc Crc32_ethernet 8 4
+  => Circuit
+    (PacketStream dom 4 ())
+    (PacketStream dom 4 ())
+fcsInserterC = fromSignals fcsInserter
+
+fcsHelper
+  :: HiddenClockResetEnable dom
+  => ( Signal dom (Vec 4 (BitVector 8))
+     , Signal dom (Maybe (PacketStreamM2S 4 ()))
+     , Signal dom PacketStreamS2M)
+  -> ( Signal dom (Maybe (PacketStreamM2S 4 ()))
+     , Signal dom Bool
+     )
+fcsHelper = mealyB fcsHelperT (FcsCopy Nothing)
+
+fcsHelperT
+  :: FcsInserterState
+  -> ( Vec 4 (BitVector 8)
+     , Maybe (PacketStreamM2S 4 ())
+     , PacketStreamS2M)
+  -> ( FcsInserterState
+     , ( Maybe (PacketStreamM2S 4 ())
+       , Bool
+       ))
+fcsHelperT
+  (FcsCopy Nothing)
+  ( _
+  , fwdIn
+  , _
+  )
+  = (FcsCopy fwdIn, (Nothing, True))
+
+fcsHelperT
+  currSt@(FcsCopy (Just maybeCache@(PacketStreamM2S{..})))
+  ( ethCrcBytes
+  , fwdIn
+  , PacketStreamS2M readyIn
+  )
+  = (nextSt, (dataOut, readyOut))
+  where
+    validIdx = fromMaybe 3 _last
+    (combined, leftover) = splitAt d4 $ appendVec (fromJust $ _last) _data ethCrcBytes
+
+    dataOut = Just $
+      if isJust _last
+      then PacketStreamM2S {
+          _data = combined
+        , _meta = ()
+        , _last = toMaybe (validIdx == 0) validIdx
+        , _abort= False
+        }
+      else maybeCache
+
+    readyOut = readyIn
+
+    nextSt = case (readyOut, _last) of
+      (False, _) -> currSt
+      (True, Just _) ->
+        FcsInsert {
+            _cachedFwd = fwdIn
+          , _bytesRemaining = validIdx
+          , _crc = leftover
+         }
+      (True, Nothing) -> FcsCopy fwdIn
+
+
+fcsHelperT
+  currSt@(FcsInsert{..})
+  ( _
+  , _
+  , PacketStreamS2M readyIn
+  )
+  = (nextSt, (dataOut, readyOut))
+  where
+    dataOut =
+      Just PacketStreamM2S {
+        _data=_crc
+      , _last=Just _bytesRemaining
+      , _meta=()
+      , _abort = False
+      }
+    readyOut = False
+    nextSt = if readyIn
+             then FcsCopy _cachedFwd
+             else currSt
+
+-- manual testing. Should be replaced with functions defined in Test.Cores.Ethernet.PacketBuffer at some point.
+
+-- charToBv = fromIntegral . ord
+
+-- chopBy :: Int -> [a] -> [[a]]
+-- chopBy _ [] = []
+-- chopBy n xs = as : chopBy n bs where (as,bs) = List.splitAt n xs
+
+
+-- fwdIn :: [Maybe (PacketStreamM2S 4 ())]
+-- fwdIn = [
+--     Just (PacketStreamM2S (unsafeFromList $ [0,0,0,0]) Nothing () False)
+--   , Just (PacketStreamM2S (unsafeFromList $ [1,0,0,0]) (Just 0) () False)]
+--   List.++ [ Just (PacketStreamM2S (unsafeFromList $ [i,0,0,0]) Nothing () False) | i <- [2..10]]
+--   List.++ [ Just (PacketStreamM2S (unsafeFromList $ [11,0,0,0]) (Just 3) () False)]
+--   List.++ [ Just (PacketStreamM2S (unsafeFromList $ [i,0,0,0]) Nothing () False) | i <- [12..15]]
+--   List.++ [ Just (PacketStreamM2S (unsafeFromList $ [0,1,0,0]) (Just 2) () False)]
+
+-- deriveHardwareCrc (Proxy @Crc32_ethernet) d8 d4
+-- fcsInserter' = exposeClockResetEnable @System fcsInserter systemClockGen resetGen enableGen
+-- fcsOut = fcsInserter' (fromList $ fwdIn, pure $ PacketStreamS2M True)
+-- fcsFromHardware = fmap sampleN (10000 + List.length fwdIn) $ snd fcsOut
+
+-- crcEngine' = exposeClockResetEnable crcEngine systemClockGen resetGen enableGen
+-- myEngine = crcEngine' $ Proxy @Crc32_ethernet
+
+
+packets = List.groupBy (\x _ -> isJust x && (isJust . _last . fromJustX $ x)) fwdIn
+expectedOut = M.join [sampleN (List.length packet + 1) $ myEngine (fromList $ True : List.repeat False) (fromList $ fmap packetStreamM2SToBv <$> packet) | packet <- packets]
+
+
---
+++ tests/Test/Cores/Ethernet/FcsInserter.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NumericUnderscores #-}
+module Test.Cores.Ethernet.FcsInserter where
+
+-- base
+import Prelude
+
+-- clash-prelude
+import qualified Clash.Prelude as C
+
+-- hedgehog
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+-- tasty
+import Test.Tasty
+import Test.Tasty.Hedgehog (HedgehogTestLimit(HedgehogTestLimit))
+import Test.Tasty.Hedgehog.Extra (testProperty)
+import Test.Tasty.TH (testGroupGenerator)
+
+-- clash-protocols
+import Protocols
+import Protocols.Hedgehog
+
+-- util module
+import Test.Cores.Ethernet.Util
+
+-- data module
+import qualified Data.List as L
+import Data.Maybe
+
+-- crc module
+import Clash.Cores.Crc
+import Clash.Cores.Ethernet.PacketStream
+import Clash.Cores.Crc.Catalog
+import Data.Proxy
+import Data.Coerce
+import Clash.Cores.Ethernet.FcsInserter
+import Clash.Explicit.Prelude (listToVecTH)
+
+genVec :: (C.KnownNat n, 1 C.<= n) => Gen a -> Gen (C.Vec n a)
+genVec gen = sequence (C.repeat gen)
+
+model :: [PacketStreamM2S 4 ()] -> [PacketStreamM2S 4 ()]
+model fragments = insertCrc =<< chunkByPacket  fragments
+
+
+packetToCrcInp
+    :: [PacketStreamM2S 4 ()] -> [C.BitVector 8]
+packetToCrcInp packet = C.head . _data <$> (chopPacket =<< packet)
+
+
+insertCrc :: [PacketStreamM2S 4 ()] -> [PacketStreamM2S 4 ()]
+insertCrc packet = L.init packet ++ [filled, trailing]
+  where
+    lastFragment = last packet
+    softwareCrc = mkSoftwareCrc (Proxy @Crc32_ethernet) C.d8
+    crcBytes = C.v2bv <$> (C.toList . C.reverse . C.unconcat C.d8 . C.bv2v . digest $ L.foldl' feed softwareCrc $ packetToCrcInp packet)
+    lastValid = fromEnum . fromJust . _last $ lastFragment
+    lastData = C.toList . _data $ lastFragment
+    (filledData, trailingData) = splitAt 4 $ take (lastValid + 1) $ lastData L.++ crcBytes
+
+    filled = lastFragment {
+        _data = foldl (C.<<+) (0 C.:> 0 C.:> 0 C.:> 0 C.:> C.Nil) filledData
+      , _last = Nothing
+    }
+    trailing = PacketStreamM2S {
+        _data = foldl (C.<<+) (0 C.:> 0 C.:> 0 C.:> 0 C.:> C.Nil) trailingData
+      , _last = _last lastFragment
+      , _meta = ()
+      , _abort = False
+    }
+
+
+-- | Test the fcsinserter
+fcsinserterTest :: Property
+fcsinserterTest =
+  propWithModelSingleDomain
+    @C.System
+    defExpectOptions
+    (Gen.list (Range.linear 0 100) genPackets)                  -- Input packets
+    (C.exposeClockResetEnable model)                            -- Desired behaviour of FcsInserter
+    (C.exposeClockResetEnable ckt)               -- Implementation of FcsInserter
+    (===)                                                       -- Property to test
+  where
+    ckt 
+      :: forall (dom :: C.Domain)
+      .  C.KnownDomain dom
+      => C.HiddenClockResetEnable dom
+      => HardwareCrc Crc32_ethernet 8 4
+      => Circuit
+        (PacketStream dom 4 ())
+        (PacketStream dom 4 ())
+    ckt = fcsInserterC
+
+    -- This generates the packets
+    genPackets =
+      PacketStreamM2S <$>
+      genVec Gen.enumBounded <*>
+      Gen.maybe Gen.enumBounded <*>
+      Gen.enumBounded <*>
+      Gen.enumBounded
+
+prop_fcsinserter_d4 :: Property
+prop_fcsinserter_d4 = fcsinserterTest 
+
+tests :: TestTree
+tests =
+    localOption (mkTimeout 12_000_000 {- 12 seconds -})
+  $ localOption (HedgehogTestLimit (Just 1_000))
+  $(testGroupGenerator)
