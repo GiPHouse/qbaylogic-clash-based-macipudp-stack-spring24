2349cf0643b7af5382e841c1f67826d8b6ea685f
JLaumen <96011116+JLaumen@users.noreply.github.com>
2024-04-18 19:09:40 +0200

    Somewhat beginning to work modulo me being able to count

--- src/Clash/Cores/Ethernet/PadPacket.hs
+++ src/Clash/Cores/Ethernet/PadPacket.hs
@@ -5,13 +5,14 @@ module Clash.Cores.Ethernet.PadPacket
 
 import Clash.Cores.Ethernet.PacketStream
 import Clash.Prelude
+import Data.List qualified as L
 import Data.Maybe
 import Protocols
 
-data PadPacketState (dataWidth :: Nat)
-  = Filling { cycles :: Index dataWidth}
+data PadPacketState
+  = Filling { cycles :: Index 64}
   | Full
-  | Padding { cycles :: Index dataWidth}
+  | Padding { cycles :: Index 64}
   deriving (Eq, Show, Generic, NFDataX)
 
 padPacket
@@ -32,9 +33,9 @@ padPacket = mealyB go s0
   where
     s0 = Filling 0
     go
-      :: PadPacketState dataWidth
+      :: PadPacketState
       -> (Maybe (PacketStreamM2S dataWidth ()), PacketStreamS2M)
-      -> ( PadPacketState dataWidth
+      -> ( PadPacketState
          , (PacketStreamS2M, Maybe (PacketStreamM2S dataWidth ()))
          )
     go st (fwdIn, PacketStreamS2M inReady)
@@ -46,7 +47,7 @@ padPacket = mealyB go s0
           -- If _last is Just, then go to Padding state if not full, otherwise go to s0
           (Filling n, Just PacketStreamM2S {..}) -> case _last of
             Nothing -> if n < maxBound - (natToNum @dataWidth) then Filling (n + (natToNum @dataWidth)) else Full
-            Just i  -> if n < maxBound - resize i then Padding (n + resize i) else s0
+            Just i  -> if n < maxBound - (natToNum @dataWidth) then Padding (maxBound - n - (natToNum @dataWidth) + 1) else s0
           -- If we are filling and don't receive any input, we keep filling
           (Filling _, Nothing) -> st
           -- If we are full, then we stay in Full state until _last is Just
@@ -58,14 +59,16 @@ padPacket = mealyB go s0
           -- While padding, it does not matter if we receive input
           (Padding n, _) -> if n > (natToNum @dataWidth) then Padding (n - (natToNum @dataWidth)) else s0
 
-        bwdOut = case st of
-          Padding _ -> PacketStreamS2M {_ready = False}
-          _         -> PacketStreamS2M {_ready = isNothing fwdIn && inReady}
+        readyOut = inReady && case st of
+          Padding _ -> False
+          _         -> isNothing fwdIn && inReady
+
+        bwdOut = PacketStreamS2M {_ready = readyOut}
 
         lastOut = case (st, fwdIn) of
           (Filling _, Just PacketStreamM2S {..}) -> if st' == s0 then _last else Nothing
           (Full, Just PacketStreamM2S {..}) -> if st' == s0 then _last else Nothing
-          (Padding n, _) -> if st' == s0 then Just n else Nothing
+          (Padding n, _) -> if st' == s0 then Just (resize (n-1)) else Nothing
           _ -> Nothing
 
         fwdOut = case (st, fwdIn) of
@@ -85,3 +88,48 @@ padPacketC
   => KnownNat dataWidth
   => Circuit (PacketStream dom dataWidth ()) (PacketStream dom dataWidth ())
 padPacketC = fromSignals padPacket
+
+payloadInp :: [Maybe (PacketStreamM2S 4 ())]
+payloadInp = [
+  Nothing
+  , Just (PacketStreamM2S (0x01 :> 0x02 :> 0x03 :> 0x04 :> Nil) (Just 0) () False)
+  , Nothing
+  , Nothing
+  , Nothing
+  , Nothing
+  -- , Nothing
+  -- , Nothing
+  , Just (PacketStreamM2S (0xAA :> 0xFF :> 0xFF :> 0xFF :> Nil) (Just 0) () False)
+  -- , Nothing
+  -- , Nothing
+  -- , Just (PacketStreamM2S (0xDE :> 0xAD :> 0x00 :> 0x00 :> Nil) (Just 1) () True)
+  -- , Just (PacketStreamM2S (0xDE :> 0xAD :> 0x00 :> 0x00 :> Nil) (Just 1) () True)
+  -- , Nothing
+  -- , Nothing
+  -- , Just (PacketStreamM2S (0xAA :> 0xAA :> 0xAA :> 0xAA :> Nil) Nothing () False)
+  -- , Just (PacketStreamM2S (0xAA :> 0xAA :> 0xAA :> 0xAA :> Nil) Nothing () False)
+  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
+  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
+  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
+  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
+  ] L.++ L.repeat Nothing
+
+sinkReadyInp :: [PacketStreamS2M]
+sinkReadyInp = fmap PacketStreamS2M ([False, True, True, True, True, True, True] L.++ (L.repeat True))
+
+clk :: Clock System
+clk = systemClockGen
+
+rst :: Reset System
+rst = systemResetGen
+
+en :: Enable dom
+en = enableGen
+
+payloadOut :: Signal System (Maybe (PacketStreamM2S 4 ()))
+sinkReadyOut :: Signal System PacketStreamS2M
+padderClk = exposeClockResetEnable padPacket clk rst en
+
+(sinkReadyOut, payloadOut) = padderClk (fromList payloadInp, fromList sinkReadyInp)
+
+sampleOut = sampleN 12 $ bundle (payloadOut, sinkReadyOut)
