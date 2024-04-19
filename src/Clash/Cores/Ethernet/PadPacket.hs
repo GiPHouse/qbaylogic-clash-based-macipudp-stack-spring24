fa0f3544dae24f3a1324c02638e581f523288b48
JLaumen <96011116+JLaumen@users.noreply.github.com>
2024-04-19 23:29:22 +0200

    Cleanup

--- src/Clash/Cores/Ethernet/PadPacket.hs
+++ src/Clash/Cores/Ethernet/PadPacket.hs
@@ -5,7 +5,6 @@ module Clash.Cores.Ethernet.PadPacket
 
 import Clash.Cores.Ethernet.PacketStream
 import Clash.Prelude
-import Data.List qualified as L
 import Data.Maybe
 import Protocols ( Circuit, fromSignals )
 
@@ -70,23 +69,10 @@ padPacket = mealyB go s0
           Filling n -> if n == maxBound && st' == s0 then Just (max (fromMaybe 0 (_last (fromJust fwdIn))) (resize calcMod)) else Nothing
           Full      -> _last =<< fwdIn
           Padding n -> if n == maxBound && st' == s0 then Just (resize calcMod) else Nothing
-        -- lastOut = case (st, fwdIn) of
-        --   (Filling n, Just PacketStreamM2S {..}) -> if st' == s0 then Just (resize (maxBound - n)) else Nothing
-        --   (Full, Just PacketStreamM2S {..}) -> if st' == s0 then _last else Nothing
-        --   (Padding n, _) -> if st' == s0 then Just (resize (n-1)) else Nothing
-        --   _ -> Nothing
+
         fwdOut = case st of
           Padding _ -> Just PacketStreamM2S{_data = repeat 0, _last = lastOut, _meta = (), _abort = False}
           _         -> if isJust fwdIn then Just (fromJust fwdIn){_last = lastOut} else fwdIn
-        -- fwdOut = case (st, fwdIn) of
-        --   (Filling _, Just PacketStreamM2S {..}) -> Just PacketStreamM2S{_data = _data, _last = lastOut, _meta = _meta, _abort = _abort}
-        --   (Full, Just PacketStreamM2S {..}) -> Just PacketStreamM2S{_data = _data, _last = lastOut, _meta = _meta, _abort = _abort}
-        --   (Padding _, _) -> Just PacketStreamM2S{_data = repeat 0, _last = lastOut, _meta = (), _abort = False}
-        -- _ -> Nothing
-
-        -- fwdOut (Filling n) (Just PacketStreamM2S {..}) _ = if st' == s0 then Nothing else Nothing
-        -- fwdOut (Filling n) Nothing _ = Nothing
-        -- fwdOut Full (Just PacketStreamM2S {..}) _ = undefined
 
 padPacketC
   :: forall (dataWidth :: Nat) (dom :: Domain).
@@ -95,48 +81,3 @@ padPacketC
   => KnownNat dataWidth
   => Circuit (PacketStream dom dataWidth ()) (PacketStream dom dataWidth ())
 padPacketC = fromSignals padPacket
-
-payloadInp :: [Maybe (PacketStreamM2S 4 ())]
-payloadInp = [
-  Nothing
-  , Just (PacketStreamM2S (0x01 :> 0x02 :> 0x03 :> 0x04 :> Nil) (Just 1) () False)
-  , Nothing
-  -- , Nothing
-  -- , Nothing
-  -- , Nothing
-  -- , Nothing
-  -- , Nothing
-  , Just (PacketStreamM2S (0xAA :> 0xFF :> 0xFF :> 0xFF :> Nil) (Nothing) () False)
-  -- , Nothing
-  -- , Nothing
-  , Just (PacketStreamM2S (0xDE :> 0xAD :> 0x00 :> 0x00 :> Nil) (Just 0) () True)
-  -- , Just (PacketStreamM2S (0xDE :> 0xAD :> 0x00 :> 0x00 :> Nil) (Just 1) () True)
-  -- , Nothing
-  -- , Nothing
-  -- , Just (PacketStreamM2S (0xAA :> 0xAA :> 0xAA :> 0xAA :> Nil) Nothing () False)
-  -- , Just (PacketStreamM2S (0xAA :> 0xAA :> 0xAA :> 0xAA :> Nil) Nothing () False)
-  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
-  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
-  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
-  -- , Just (PacketStreamM2S (0x55 :> 0x55 :> 0x55 :> 0x55 :> Nil) (Just 3) () False)
-  ] L.++ L.repeat Nothing
-
-sinkReadyInp :: [PacketStreamS2M]
-sinkReadyInp = fmap PacketStreamS2M ([False, True, True, True, True, True, True] L.++ (L.repeat True))
-
-clk :: Clock System
-clk = systemClockGen
-
-rst :: Reset System
-rst = systemResetGen
-
-en :: Enable dom
-en = enableGen
-
-payloadOut :: Signal System (Maybe (PacketStreamM2S 4 ()))
-sinkReadyOut :: Signal System PacketStreamS2M
-padderClk = exposeClockResetEnable padPacket clk rst en
-
-(sinkReadyOut, payloadOut) = padderClk (fromList payloadInp, fromList sinkReadyInp)
-
-sampleOut = sampleN 12 $ bundle (payloadOut, sinkReadyOut)
