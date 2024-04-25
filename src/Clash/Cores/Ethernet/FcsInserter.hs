b57b8bcc82e03b8d14574ce06135968adb3622e2
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2024-04-25 14:29:10 +0200

    put vector util functions in Clash.Sized.Vector.Extra

--- src/Clash/Cores/Ethernet/FcsInserter.hs
+++ src/Clash/Cores/Ethernet/FcsInserter.hs
@@ -25,6 +25,9 @@ import Data.Data ( Proxy(Proxy) )
 -- maybe
 import Data.Maybe
 
+-- vector
+import Clash.Sized.Vector.Extra
+
 -- protocols
 import Protocols
 
@@ -35,38 +38,7 @@ fragment2bv
   -> (Index dataWidth, Vec dataWidth (BitVector 8))
 fragment2bv (PacketStreamM2S{..}) = (fromMaybe maxBound _last, _data)
 
--- | Like 'take' but uses a 'Data.Type.Ord.<=' constraint
-takeLe
-  :: forall (n :: Nat)
-            (m :: Nat)
-            a
-   . n <= m
-  => SNat n
-  -- ^ How many elements to take
-  -> Vec m a
-  -- ^ input vector
-  -> Vec n a
-takeLe SNat vs = leToPlus @n @m $ takeI vs
 
--- | Take the first 'valid' elements of 'xs', append 'ys', then pad with 0s
-appendVec
-  :: forall n m a
-   . KnownNat n
-  => Num a
-  => Index n
-  -> Vec n a
-  -> Vec m a
-  -> Vec (n + m) a
-appendVec valid xs ys = results !! valid
-  where
-    go :: forall l. SNat l -> Vec (n + m) a
-    go l@SNat = let f = addSNat l d1 in case compareSNat f (SNat @n) of
-                  SNatLE -> takeLe (addSNat l d1) xs ++ ys ++ extra
-                   where
-                     extra :: Vec (n - (l + 1)) a
-                     extra = repeat 0
-                  _ -> error "appendVec: Absurd"
-    results = smap (\s _ -> go s) xs
 
 -- | States of the FcsInserter
 data FcsInserterState dataWidth
