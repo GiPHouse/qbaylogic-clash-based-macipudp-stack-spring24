b7883dfb497ebf4cc7e54fc80bea7e5a629a90ac
Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
2024-04-25 15:29:26 +0200

    make internetchecksum datawidth general

--- src/Clash/Cores/Ethernet/InternetChecksum.hs
+++ src/Clash/Cores/Ethernet/InternetChecksum.hs
@@ -9,19 +9,22 @@ import Data.Maybe
 -- The checksum and reset are delayed by one clock cycle.
 -- Keep in mind that if "reset" is True in the input tuple, the checksum is reset to 0 the next cycle so the value of the bitvector is disgarded
 internetChecksum
-  :: forall (dom :: Domain).
+  :: forall (dom :: Domain) (dataWidth :: Nat).
   HiddenClockResetEnable dom
-  => Signal dom (Maybe (BitVector 16, Bool))
+  => KnownNat dataWidth
+  => 1 <= dataWidth
+  => Signal dom (Maybe (BitVector dataWidth, Bool))
   -- ^ Input data, adds the first data point of the checksum, if the second element of the tuple is True, the current checksum is reset to 0 the next cycle
-  -> Signal dom (BitVector 16)
+  -> Signal dom (BitVector dataWidth)
  -- ^ Resulting checksum
 internetChecksum inputM = checkSumWithCarry
   where
     (inpX, resetX) = unbundle $ fromJustX <$> inputM
 
-    checkSum :: Signal dom (BitVector 17)
+    checkSum :: Signal dom (BitVector (dataWidth + 1))
     checkSum = regEn 0 (isJust <$> inputM) $ mux resetX 0 nextCheckSum
 
-    (fmap zeroExtend -> carry, truncated) = unbundle $ split <$> checkSum
+    (fmap (zeroExtend :: BitVector 1 -> BitVector ((dataWidth - 1) + 1)) -> carry, truncated) = unbundle $ split <$> checkSum
+
     checkSumWithCarry = carry + truncated
     nextCheckSum = add <$> inpX <*> checkSumWithCarry
