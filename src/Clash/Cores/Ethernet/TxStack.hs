55c83ca99ddfbf6b41e4abaa471d024790aa60dc
t-wallet <tw.personal@tutanota.com>
2024-05-01 11:30:10 +0200

    preamble inserter implementation + tests (#98)
    * preamble inserter implementation + tests, ethernet TX stack is now fully functional with a static EthernetHeader

--- src/Clash/Cores/Ethernet/TxStack.hs
+++ src/Clash/Cores/Ethernet/TxStack.hs
@@ -1,13 +1,24 @@
+{-# language FlexibleContexts #-}
+
 module Clash.Cores.Ethernet.TxStack
   ( txStack
   ) where
 
+import Clash.Cores.Crc
+import Clash.Cores.Crc.Catalog
+
 import Clash.Cores.Ethernet.AsyncFIFO ( asyncFifoC )
-import Clash.Cores.Ethernet.DownConverter
+import Clash.Cores.Ethernet.DownConverter ( downConverterC )
+import Clash.Cores.Ethernet.EthernetTypes
+import Clash.Cores.Ethernet.FcsInserter ( fcsInserterC )
 import Clash.Cores.Ethernet.InterpacketGapInserter ( interpacketGapInserterC )
-import Clash.Cores.Ethernet.PacketStream
+import Clash.Cores.Ethernet.Packetizer ( packetizerC )
+import Clash.Cores.Ethernet.PacketStream ( PacketStream )
+import Clash.Cores.Ethernet.PaddingInserter ( paddingInserterC )
+import Clash.Cores.Ethernet.PreambleInserter ( preambleInserterC )
 import Clash.Prelude
 import Protocols
+
 
 -- | Processes bytes to send over ethernet
 txStack
@@ -16,13 +27,26 @@ txStack
      , 1 <= dataWidth
      , HiddenClockResetEnable dom
      , KnownDomain domEth
+     , HardwareCrc Crc32_ethernet 8 dataWidth
      )
   => Clock domEth
   -> Reset domEth
   -> Enable domEth
   -> Circuit (PacketStream dom dataWidth ()) (PacketStream domEth 1 ())
-txStack ethClk ethRst ethEn = asyncFifoC' |> downConverterC' |> interpacketGapInserterC' d12
-  where
-    asyncFifoC' = asyncFifoC d4 hasClock hasReset hasEnable ethClk ethRst ethEn
-    downConverterC' = exposeClockResetEnable downConverterC ethClk ethRst ethEn
-    interpacketGapInserterC' = exposeClockResetEnable interpacketGapInserterC ethClk ethRst ethEn
+txStack ethClk ethRst ethEn
+  =  packetizerC (const ()) (const header)
+  |> paddingInserterC d60
+  |> fcsInserterC
+  |> preambleInserterC
+  |> asyncFifoC'
+  |> downConverterC'
+  |> interpacketGapInserterC' d12
+    where
+      header = EthernetHeader {
+        _macDst = MacAddress $ repeat 0xFF,
+        _macSrc = MacAddress $ repeat 0xFF,
+        _etherType = 0xFFFF
+      }
+      asyncFifoC' = asyncFifoC d4 hasClock hasReset hasEnable ethClk ethRst ethEn
+      downConverterC' = exposeClockResetEnable downConverterC ethClk ethRst ethEn
+      interpacketGapInserterC' = exposeClockResetEnable interpacketGapInserterC ethClk ethRst ethEn
