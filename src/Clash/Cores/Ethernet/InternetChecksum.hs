9746b4f5a5e973bd45a62dec8b9bf08dc3ad9c02
Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
2024-05-06 16:25:15 +0200

    add simple checksum version and initial test

--- src/Clash/Cores/Ethernet/InternetChecksum.hs
+++ src/Clash/Cores/Ethernet/InternetChecksum.hs
@@ -3,11 +3,13 @@ Module      : Clash.Cores.Ethernet.InternetChecksum
 Description : Functions for computing the RFC1071 internet checksum
 -}
 module Clash.Cores.Ethernet.InternetChecksum
-  ( internetChecksum
+  ( internetChecksum,
+    reduceToInternetChecksum
   ) where
 
 import Clash.Prelude
 import Data.Maybe
+import Prelude qualified as P
 
 -- | Computes the uncomplimented internet checksum of a stream of 16-bit words
 -- according to https://datatracker.ietf.org/doc/html/rfc1071. The checksum and
@@ -46,3 +48,26 @@ internetChecksum :: forall (dom :: Domain).
   -> Signal dom (BitVector 16)
   -- ^ Resulting checksum
 internetChecksum = generalChecksum
+
+calcChecksum :: BitVector 16 -> BitVector 16 -> BitVector 16
+calcChecksum bvA bvB = carry + truncated
+  where
+    (zeroExtend -> carry, truncated) = split checkSum
+    checkSum :: BitVector 17
+    checkSum = add bvA bvB
+
+
+reduceToInternetChecksum ::
+  forall (dom :: Domain) (width :: Nat).
+  HiddenClockResetEnable dom
+  => 1 <= width
+  => Signal dom (Maybe (Vec width (BitVector 16), Bool))
+  -- ^ Input data, adds the first data point of the checksum, if the second element of the tuple is True, the current checksum is reset to 0 the next cycle
+  -> Signal dom (BitVector 16)
+  -- ^ Resulting checksum
+reduceToInternetChecksum inputM = checkSum
+  where
+    checkSum = regEn 0 (isJust <$> inputM) $ mux resetX 0 checksumResult
+    (inpX, resetX) = unbundle $ fromJustX <$> inputM
+    checksumResult = fold calcChecksum <$> input
+    input = (++) <$> (singleton <$> checkSum) <*> inpX
