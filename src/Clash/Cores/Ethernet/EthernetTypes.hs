75ac1b89b89cc9e0dceef5918e04632cc0b88325
Daan Weessies <daan.weessies@gmail.com>
2024-05-10 18:35:18 +0200

    Initial commit, have a typechecked version

--- src/Clash/Cores/Ethernet/EthernetTypes.hs
+++ src/Clash/Cores/Ethernet/EthernetTypes.hs
@@ -9,10 +9,19 @@ module Clash.Cores.Ethernet.EthernetTypes
   , broadcastMac
   , preamble
   , startFrameDelimiter
+  , IPv4Address
+  , toEthernetC
   ) where
 
 import Clash.Prelude
+
+import Protocols
+
+import Clash.Cores.Ethernet.PacketStream
 import Control.DeepSeq ( NFData )
+import Data.Bifunctor qualified as B
+import Data.Tuple ( swap )
+
 
 -- | Stores a MAC address, which is always 6 bytes long.
 newtype MacAddress = MacAddress (Vec 6 (BitVector 8))
@@ -40,3 +49,22 @@ startFrameDelimiter = 0xD5
 -- | Broadcast MAC address.
 broadcastMac :: MacAddress
 broadcastMac = MacAddress (repeat 0xFF)
+
+type IPv4Address = Vec 4 (BitVector 8)
+
+-- | Convert an IPv4Address to the corresponding EthernetHeader.
+toEthernetC :: HiddenClockResetEnable dom
+  => Signal dom MacAddress
+  -> Circuit (PacketStream dom n IPv4Address) (PacketStream dom n EthernetHeader)
+toEthernetC macSrc = Circuit (swap . unbundle . helper macSrc . bundle)
+  where
+    helper mac pkt = go <$> mac <*> pkt
+    go mac = B.first $ fmap $ fmap (toEthernet mac)
+
+
+toEthernet :: MacAddress -> IPv4Address -> EthernetHeader
+toEthernet macSrc _ = EthernetHeader {
+  _macDst = MacAddress (repeat 0x37),
+  _macSrc = macSrc,
+  _etherType = 0x0800 -- IPv4 EtherType
+}
