56686da304c28825127c559a0fe323c2fb1c81ce
Cato van Ojen <cato.vanojen@student.ru.nl>
2024-05-16 11:47:00 +0200

    - Remove newlines after module definition - Split comments into multiple lines

--- src/Clash/Cores/Ethernet/InternetChecksum.hs
+++ src/Clash/Cores/Ethernet/InternetChecksum.hs
@@ -2,8 +2,6 @@
 Module      : Clash.Cores.Ethernet.InternetChecksum
 Description : Functions for computing the RFC1071 internet checksum
 -}
-
-
 module Clash.Cores.Ethernet.InternetChecksum
   ( internetChecksum,
     reduceToInternetChecksum,
@@ -17,14 +15,17 @@ import Data.Maybe
 import Data.Proxy
 import Data.Type.Equality
 
--- | computes the un-complimented internet checksum of a stream of 16-bit words according to https://datatracker.ietf.org/doc/html/rfc1071
+-- | computes the un-complimented internet checksum of a stream of 16-bit words
+-- according to https://datatracker.ietf.org/doc/html/rfc1071
 -- The checksum and reset are delayed by one clock cycle.
--- Keep in mind that if "reset" is True in the input tuple, the checksum is reset to 0 the next cycle so the value of the bitvector is disgarded
+-- Keep in mind that if "reset" is True in the input tuple, the checksum is 
+-- reset to 0 the next cycle so the value of the bitvector is disgarded
 internetChecksum
   :: forall (dom :: Domain).
   HiddenClockResetEnable dom
   => Signal dom (Maybe (BitVector 16, Bool))
-  -- ^ Input data, adds the first data point of the checksum, if the second element of the tuple is True, the current checksum is reset to 0 the next cycle
+  -- ^ Input data, adds the first data point of the checksum, if the second 
+  -- element of the tuple is True, the current checksum is reset to 0 the next cycle
   -> Signal dom (BitVector 16)
  -- ^ Resulting checksum
 internetChecksum inputM = checkSumWithCarry
@@ -46,13 +47,15 @@ calcChecksum bvA bvB = carry + truncated
     checkSum :: BitVector 17
     checkSum = add bvA bvB
 
--- | Computes the internetChecksum of a vector of 16 bit words. Compared to internetChecksum this is quicker as you can load multiple words per cycle.
+-- | Computes the internetChecksum of a vector of 16 bit words. Compared to 
+-- internetChecksum this is quicker as you can load multiple words per cycle.
 reduceToInternetChecksum ::
   forall (dom :: Domain) (width :: Nat).
   HiddenClockResetEnable dom
   => 1 <= width
   => Signal dom (Maybe (Vec width (BitVector 16), Bool))
-  -- ^ Input data, adds the first data point of the checksum, if the second element of the tuple is True, the current checksum is reset to 0 the next cycle
+  -- ^ Input data, adds the first data point of the checksum, if the second 
+  -- element of the tuple is True, the current checksum is reset to 0 the next cycle
   -> Signal dom (BitVector 16)
   -- ^ Resulting checksum
 reduceToInternetChecksum inputM = checkSum
@@ -68,7 +71,8 @@ pipelinedInternetChecksum ::
   => 1 <= width
   => KnownNat width
   => Signal dom (Maybe (Vec width (BitVector 16), Bool))
-  -- ^ Input data, adds the first data point of the checksum, if the second element of the tuple is True, the current checksum is reset to 0 the next cycle
+  -- ^ Input data, adds the first data point of the checksum, if the second 
+  -- element of the tuple is True, the current checksum is reset to 0 the next cycle
   -> Signal dom (BitVector 16)
   -- ^ Resulting checksum
 pipelinedInternetChecksum inputM = checkSum
