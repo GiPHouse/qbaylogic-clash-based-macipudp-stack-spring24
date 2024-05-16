25e754944dc5e4d26d820ea6a253f2fc4d78183b
Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
2024-05-16 15:59:16 +0200

    add 1 <= n resttriction to foldchecksum, rename to foldpipeline and make generic based on function
    Co-authored-by: Cato van Ojen <Baublesaurus@users.noreply.github.com>

--- src/Clash/Cores/Ethernet/InternetChecksum.hs
+++ src/Clash/Cores/Ethernet/InternetChecksum.hs
@@ -18,13 +18,13 @@ import Data.Type.Equality
 -- | computes the un-complimented internet checksum of a stream of 16-bit words
 -- according to https://datatracker.ietf.org/doc/html/rfc1071
 -- The checksum and reset are delayed by one clock cycle.
--- Keep in mind that if "reset" is True in the input tuple, the checksum is 
+-- Keep in mind that if "reset" is True in the input tuple, the checksum is
 -- reset to 0 the next cycle so the value of the bitvector is disgarded
 internetChecksum
   :: forall (dom :: Domain).
   HiddenClockResetEnable dom
   => Signal dom (Maybe (BitVector 16, Bool))
-  -- ^ Input data, adds the first data point of the checksum, if the second 
+  -- ^ Input data, adds the first data point of the checksum, if the second
   -- element of the tuple is True, the current checksum is reset to 0 the next cycle
   -> Signal dom (BitVector 16)
  -- ^ Resulting checksum
@@ -47,14 +47,14 @@ calcChecksum bvA bvB = carry + truncated
     checkSum :: BitVector 17
     checkSum = add bvA bvB
 
--- | Computes the internetChecksum of a vector of 16 bit words. Compared to 
--- internetChecksum this is quicker as you can load multiple words per cycle.
+-- | Computes the internetChecksum of a vector of 16 bit words. Compared to
+-- internetChecksum this is quicker as you can load multiple words per cycle
 reduceToInternetChecksum ::
   forall (dom :: Domain) (width :: Nat).
   HiddenClockResetEnable dom
   => 1 <= width
   => Signal dom (Maybe (Vec width (BitVector 16), Bool))
-  -- ^ Input data, adds the first data point of the checksum, if the second 
+  -- ^ Input data, adds the first data point of the checksum, if the second
   -- element of the tuple is True, the current checksum is reset to 0 the next cycle
   -> Signal dom (BitVector 16)
   -- ^ Resulting checksum
@@ -71,7 +71,7 @@ pipelinedInternetChecksum ::
   => 1 <= width
   => KnownNat width
   => Signal dom (Maybe (Vec width (BitVector 16), Bool))
-  -- ^ Input data, adds the first data point of the checksum, if the second 
+  -- ^ Input data, adds the first data point of the checksum, if the second
   -- element of the tuple is True, the current checksum is reset to 0 the next cycle
   -> Signal dom (BitVector 16)
   -- ^ Resulting checksum
@@ -79,26 +79,33 @@ pipelinedInternetChecksum inputM = checkSum
   where
     checkSum = register 0 $ mux reset 0 checksumResult
     (inp, resetInp) = unbundle $ fromMaybe (repeat 0, False) <$> inputM
-    checksumResult = calcChecksum <$> foldChecksum inp <*> checkSum
+    checksumResult = calcChecksum <$> foldPipeline calcChecksum inp <*> checkSum
     reset = U.registerN (SNat :: SNat (PipelineDelay width-1)) False resetInp
 
 
-foldChecksum ::
+foldPipeline ::
   forall (dom :: Domain) (n::Nat).
   HiddenClockResetEnable dom 
   => KnownNat n
-  => Signal dom (Vec n (BitVector 16))
+  => 1 <= n 
+  => (BitVector 16 -> BitVector 16 -> BitVector 16) 
+  -> Signal dom (Vec n (BitVector 16))
   -> Signal dom (BitVector 16)
-foldChecksum inp = case sameNat (Proxy :: Proxy n ) (Proxy :: Proxy 1) of
-    Just Refl -> head <$> inp
-    Nothing -> foldChecksum foldValues
+foldPipeline func inp  = case (
+    sameNat (Proxy :: Proxy n) (Proxy :: Proxy 1), 
+    compareSNat d1 (SNat @(n `Div` 2 + n `Mod` 2))
+    ) of
+    (_, SNatGT) -> error "n `Div` 2 + n `Mod` 2 <= 1 impossible"
+    (Just Refl, _) -> head <$> inp
+    (Nothing, SNatLE) -> foldPipeline func foldValues
       where
         foldValues :: Signal dom (Vec (n `Div` 2 + n `Mod` 2) (BitVector 16))
-        foldValues = case (
+        foldValues = 
+          case (
               compareSNat (SNat @(n `Mod` 2)) d1,
               sameNat (Proxy :: Proxy (2 * (n `Div` 2) + n `Mod` 2)) (Proxy:: Proxy n)
             ) of
-          (SNatLE, Just Refl) -> (step @(n `Div` 2) @(n `Mod` 2)) (Proxy::Proxy (n `Div` 2))inp
+          (SNatLE, Just Refl) -> (step @(n `Div` 2) @(n `Mod` 2)) (Proxy::Proxy (n `Div` 2)) inp
           _ -> error "'n % 2 > 1', or '2*(n/2)+n%2 != x': impossible"
 
         step ::
