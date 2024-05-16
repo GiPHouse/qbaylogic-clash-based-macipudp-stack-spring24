fd0e807a920a8324c8d47b91c99f25d34870a422
Cato van Ojen <cato.vanojen@student.ru.nl>
2024-05-16 23:44:30 +0200

    Moved step out of the where clause
    Co-authored-by: Jasmijn <JasmijnB@users.noreply.github.com>

--- src/Clash/Sized/Vector/Extra.hs
+++ src/Clash/Sized/Vector/Extra.hs
@@ -41,61 +41,67 @@ appendVec valid xs ys = results !! valid
                   _ -> error "appendVec: Absurd"
     results = smap (\s _ -> go s) xs
 
--- | Fold a vector of 'n' elements into a single element using a binary function. Between every "layer" of the fold, there is a register
--- | This means there is a latency between input and output of 'PipelineDelay width' cycles. This is equal to CLog2(width) + 1
+-- | Fold a vector of 'n' elements into a single element using a binary function.
+-- | Between every "layer" of the fold, there is a register
+-- | This means there is a latency between input and output of 'PipelineDelay width' cycles.
+-- | This is equal to CLog2(width) + 1
 foldPipeline ::
-  forall (dom :: Domain) (n::Nat) (a::Type).
+  forall (dom :: Domain) (n :: Nat) (a :: Type).
   HiddenClockResetEnable dom
   => KnownNat n
   => 1 <= n
   => NFDataX a
-  => (a -> a -> a)
+  => a
+  -> (a -> a -> a)
   -- ^ Associative binary operation to apply
   -> Signal dom (Vec n a)
   -- ^ Input values
   -> Signal dom a
-foldPipeline f inp  = case (
-    sameNat (Proxy :: Proxy n) (Proxy :: Proxy 1),
-    compareSNat d1 (SNat @(n `Div` 2 + n `Mod` 2))
-    ) of
+foldPipeline initial f inp  = case (nIs1, foldWidthBiggerThan1) of
     (_, SNatGT) -> error "n `Div` 2 + n `Mod` 2 <= 1 impossible"
     (Just Refl, _) -> head <$> inp
-    (Nothing, SNatLE) -> foldPipeline f foldValues
-      where
-        foldValues :: Signal dom (Vec (n `Div` 2 + n `Mod` 2) a)
-        foldValues =
-          case (
-              compareSNat (SNat @(n `Mod` 2)) d1,
-              sameNat (Proxy :: Proxy (2 * (n `Div` 2) + n `Mod` 2)) (Proxy:: Proxy n)
-            ) of
-          (SNatLE, Just Refl) -> (step @(n `Div` 2) @(n `Mod` 2)) (Proxy::Proxy (n `Div` 2)) inp
-          _ -> error "'n % 2 > 1', or '2*(n/2)+n%2 != x': impossible"
+    (Nothing, SNatLE) -> foldPipeline initial f foldValues
+    where
+      nIs1 = sameNat (Proxy :: Proxy n) (Proxy :: Proxy 1)
+      foldWidthBiggerThan1 = compareSNat d1 (SNat @(n `Div` 2 + n `Mod` 2))
 
-        step ::
-          forall (m::Nat) (p::Nat).
-          KnownNat m
-          => KnownNat p
-          => p <= 1
-          => Proxy m
-          -> Signal dom (Vec (2 * m + p) a)
-          -> Signal dom (Vec (m + p) a)
-        step _ inps = case (
-            sameNat (Proxy :: Proxy p) (Proxy :: Proxy 0),
-            sameNat (Proxy :: Proxy p) (Proxy :: Proxy 1)
-          ) of
-          (Just Refl, Nothing) -> regVec $ layerCalc inps
-          (Nothing, Just Refl) -> regVec $ (++) <$> (singleton . head <$> inps) <*> layerCalc (tail <$> inps)
-          _ -> error "p > 1 impossible"
-          where
-            layerCalc :: Signal dom (Vec (2*m) a) -> Signal dom (Vec m a)
-            layerCalc inl = register (repeat undefined) $ fmap (fmap applyF . unconcatI) inl
+      foldValues :: Signal dom (Vec (n `Div` 2 + n `Mod` 2) a)
+      foldValues =
+        case (atLeast1mod2, nEqualsN) of
+        (SNatLE, Just Refl) -> (step @(n `Div` 2) @(n `Mod` 2)) (Proxy::Proxy (n `Div` 2)) initial f inp
+        _ -> error "'n % 2 > 1', or '2*(n/2)+n%2 != x': impossible"
+      
+      atLeast1mod2 = compareSNat (SNat @(n `Mod` 2)) d1
+      nEqualsN = sameNat (Proxy :: Proxy (2 * (n `Div` 2) + n `Mod` 2)) (Proxy :: Proxy n)
 
-            applyF :: Vec 2 a -> a
-            applyF (a :> b :> _) = f a b
-            applyF _ = error "calcChecksum2: impossible"
+step :: forall (m :: Nat) (p :: Nat) (dom :: Domain) (a :: Type).
+  HiddenClockResetEnable dom
+  => KnownNat m
+  => KnownNat p
+  => p <= 1
+  => NFDataX a
+  => Proxy m
+  -> a
+  -> (a -> a -> a)
+  -> Signal dom (Vec (2 * m + p) a)
+  -> Signal dom (Vec (m + p) a)
+step _ initial f inps = case (
+    sameNat (Proxy :: Proxy p) (Proxy :: Proxy 0),
+    sameNat (Proxy :: Proxy p) (Proxy :: Proxy 1)
+  ) of
+  (Just Refl, Nothing) -> regVec $ layerCalc inps
+  (Nothing, Just Refl) -> regVec $ (++) <$> (singleton . head <$> inps) <*> layerCalc (tail <$> inps)
+  _ -> error "p > 1 impossible"
+  where
+    layerCalc :: Signal dom (Vec (2*m) a) -> Signal dom (Vec m a)
+    layerCalc = fmap (fmap applyF . unconcatI)
 
-            regVec :: KnownNat q => Signal dom (Vec q a) -> Signal dom (Vec q a)
-            regVec vs = bundle (register undefined <$> unbundle vs)
+    applyF :: Vec 2 a -> a
+    applyF (a :> b :> _) = f a b
+    applyF _ = error "calcChecksum2: impossible"
+
+    regVec :: KnownNat q => Signal dom (Vec q a) -> Signal dom (Vec q a)
+    regVec vs = bundle $ register initial <$> unbundle vs
 
 -- | The latency of the pipeline
 type PipelineLatency (n :: Nat) = 1 + CLog 2 n
