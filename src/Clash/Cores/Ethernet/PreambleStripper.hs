11c1ac4a312f7b560a8dffd4161f557bcefa3a48
t-wallet <tw.personal@tutanota.com>
2024-05-21 12:33:20 +0200

    packetstream metadata utilities

--- src/Clash/Cores/Ethernet/PreambleStripper.hs
+++ src/Clash/Cores/Ethernet/PreambleStripper.hs
@@ -13,28 +13,21 @@ import Protocols
 import Clash.Cores.Ethernet.Depacketizer
 import Clash.Cores.Ethernet.EthernetTypes
 import Clash.Cores.Ethernet.PacketStream
-import Clash.Cores.Ethernet.Util
-
-import Data.Maybe ( isNothing )
 
 
--- | Strips the incoming PacketStream of the preamble and SFD. Drops a packet only if the SFD is not correct,
---   the circuit does not check if the preamble itself matches for efficiency reasons.
-preambleStripperC :: forall (dom :: Domain) (dataWidth :: Nat) .
-  ( KnownDomain dom
-  , HiddenClockResetEnable dom
-  , KnownNat dataWidth
-  , 1 <= dataWidth)
+-- | Strips the incoming PacketStream of the preamble and SFD. Drops a packet only
+--   if the SFD is not correct, the circuit does not check if the preamble itself
+--   matches for efficiency reasons.
+preambleStripperC
+  :: forall
+     (dom :: Domain)
+     (dataWidth :: Nat)
+   . HiddenClockResetEnable dom
+  => KnownNat dataWidth
+  => 1 <= dataWidth
   => Circuit (PacketStream dom dataWidth ()) (PacketStream dom dataWidth ())
 -- Only put the SFD in the metadata for efficiency reasons.
-preambleStripperC = depacketizerC (\p _ -> last (p :: Preamble)) |> fromSignals ckt
-  where
-    ckt (fwdIn, bwdIn) = (bwdOut, fwdOut)
-      where
-        -- It's illegal to look at bwdIn when you are sending out a Nothing
-        -- since the test framework drives undefined acks randomly if fwdOut
-        -- is Nothing. So if we drive a Nothing force a True on bwdOut.
-        bwdOut = PacketStreamS2M <$> ((isNothing <$> fwdOut) .||. _ready <$> bwdIn)
-        -- We send Nothing if the SFD is not 0xD5. That is, we drop the packet.
-        dropBadSfd p = toMaybe (_meta p == startFrameDelimiter) (p { _meta = () })
-        fwdOut = (dropBadSfd =<<) <$> fwdIn
+preambleStripperC =
+  depacketizerC (\(p :: Preamble) _ -> last p)
+  |> filterMeta (== startFrameDelimiter)
+  |> mapMeta (const ())
